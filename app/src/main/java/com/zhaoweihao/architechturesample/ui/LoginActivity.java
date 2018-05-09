74e627ddc613c4fd5110621b77032680c2e6d341
TanXinKui <2647587557@qq.com>
2018-05-09 15:41:02 +0800

    添加了一个toast

--- app/src/main/java/com/zhaoweihao/architechturesample/ui/LoginActivity.java
+++ app/src/main/java/com/zhaoweihao/architechturesample/ui/LoginActivity.java
@@ -2,6 +2,7 @@ package com.zhaoweihao.architechturesample.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.zhaoweihao.architechturesample.R;
 
@@ -11,5 +12,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Toast.makeText(this, "aaa", Toast.LENGTH_SHORT).show();
     }
 }
