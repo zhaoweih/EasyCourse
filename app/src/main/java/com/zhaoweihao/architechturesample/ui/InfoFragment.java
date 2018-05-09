0cb2b9e62ffd503e259d29ff75ca5674b00472aa
zhaoweihaoChina <1028410337@qq.com>
2018-05-09 14:44:33 +0800

    添加登录界面

--- app/src/main/java/com/zhaoweihao/architechturesample/ui/InfoFragment.java
+++ app/src/main/java/com/zhaoweihao/architechturesample/ui/InfoFragment.java
@@ -1,12 +1,45 @@
 package com.zhaoweihao.architechturesample.ui;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.zhaoweihao.architechturesample.R;
 
 public class InfoFragment extends Fragment {
+
+    //View的引用
+    private Button mButton;
 
     public InfoFragment() {
 
     }
 
     public static InfoFragment newInstance() { return new InfoFragment(); }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_info, container, false);
+        initViews(view);
+
+        mButton.setOnClickListener( v -> {
+            Intent intent = new Intent(getActivity(),LoginActivity.class);
+            startActivity(intent);
+        });
+
+
+        return view;
+    }
+
+    public void initViews(View view) {
+        mButton = view.findViewById(R.id.btn_login);
+    }
 }
+
