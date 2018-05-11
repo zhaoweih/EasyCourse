d1b0115eb972f31976fcdd2cc5c9e365dea5d102
zhaoweihaoChina <1028410337@qq.com>
2018-05-11 15:09:41 +0800

    增加例子

---
+++ app/src/main/java/com/zhaoweihao/architechturesample/data/Login.java
@@ -0,0 +1,26 @@
+package com.zhaoweihao.architechturesample.data;
+
+public class Login {
+    /**
+     * @username 登录需要的用户名
+     * @password 登录需要的密码
+     */
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ app/src/main/java/com/zhaoweihao/architechturesample/util/HttpUtil.java
@@ -0,0 +1,38 @@
+package com.zhaoweihao.architechturesample.util;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
+public class HttpUtil {
+
+    /**
+     * 网络工具类
+     * 提供发送get请求和post请求的静态方法
+     */
+
+    private static final String prefix = "http://zhaoweihao.com:9000/api/";
+
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    public static void sendGetRequest(String address,okhttp3.Callback callback) {
+        address = prefix + address;
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(address)
+                .build();
+        client.newCall(request).enqueue(callback);
+    }
+    public static void sendPostRequest(String address, String json, okhttp3.Callback callback) {
+        address = prefix + address;
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(address)
+                .post(body)
+                .build();
+        client.newCall(request).enqueue(callback);
+    }
+
+}
