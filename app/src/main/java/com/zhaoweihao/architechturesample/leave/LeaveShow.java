f5a7a8a5cb17f1957273def0efcc1563454227e7
TanXinKui <2647587557@qq.com>
2018-05-17 23:35:23 +0800

    请假提交

---
+++ app/src/main/java/com/zhaoweihao/architechturesample/leave/LeaveShow.java
@@ -0,0 +1,15 @@
+package com.zhaoweihao.architechturesample.leave;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zhaoweihao.architechturesample.R;
+
+public class LeaveShow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leave_show);
+    }
+}
