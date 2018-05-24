8ed23539e403ac1b19c65537b93896725d0e8a91
zhaoweihaoChina <1028410337@qq.com>
2018-05-24 20:29:47 +0800

    在UtilActivity增加上传图片

--- app/src/main/java/com/zhaoweihao/architechturesample/util/HttpUtil.java
+++ app/src/main/java/com/zhaoweihao/architechturesample/util/HttpUtil.java
@@ -1,6 +1,10 @@
 package com.zhaoweihao.architechturesample.util;
 
+import java.io.File;
+
+import okhttp3.Callback;
 import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -31,6 +35,18 @@ public class HttpUtil {
         Request request = new Request.Builder()
                 .url(address)
                 .post(body)
+                .build();
+        client.newCall(request).enqueue(callback);
+    }
+    public static void sendPostRequestWithFile(String address, File file, okhttp3.Callback callback) {
+        address = prefix + address;
+        OkHttpClient client = new OkHttpClient();
+        MediaType MEDIA_TYPE_JPEG = MediaType.parse("image/jpeg");
+        MultipartBody mb = new MultipartBody.Builder().setType(MultipartBody.FORM)
+                .addFormDataPart("file", file.getName(), RequestBody.create(MEDIA_TYPE_JPEG, file)).build();
+        Request request = new Request.Builder()
+                .url(address)
+                .post(mb)
                 .build();
         client.newCall(request).enqueue(callback);
     }
