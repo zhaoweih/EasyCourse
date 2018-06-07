1ea54993d6fbb3d244eac51cd0bef66a2a77fc7a
zhaoweihaoChina <1028410337@qq.com>
2018-06-07 19:26:21 +0800

    增加返回按钮

--- app/src/main/java/com/zhaoweihao/architechturesample/course/CourseActivity.java
+++ app/src/main/java/com/zhaoweihao/architechturesample/course/CourseActivity.java
@@ -3,6 +3,7 @@ package com.zhaoweihao.architechturesample.course;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 
 import com.zhaoweihao.architechturesample.R;
@@ -12,6 +13,7 @@ import com.zhaoweihao.architechturesample.seat.EnterActivity;
 import com.zhaoweihao.architechturesample.vote.ShowActivity;
 
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -23,43 +25,53 @@ public class CourseActivity extends AppCompatActivity {
 
     private int courseId;
 
-    @OnClick(R.id.check_stu) void checkStu() {
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @OnClick(R.id.check_stu)
+    void checkStu() {
         Intent intent = new Intent(this, QuerySelectCourseActivity.class);
         intent.putExtra("courseId", courseId);
         startActivity(intent);
     }
 
-    @OnClick(R.id.check_noti) void checkNoti() {
+    @OnClick(R.id.check_noti)
+    void checkNoti() {
         Intent intent = new Intent(this, QueryNotiActivity.class);
         intent.putExtra("courseId", courseId);
         startActivity(intent);
     }
 
-    @OnClick(R.id.to_vote) void toVote() {
+    @OnClick(R.id.to_vote)
+    void toVote() {
         Intent intent = new Intent(this, ShowActivity.class);
         intent.putExtra("courseId", courseId);
         startActivity(intent);
     }
 
-    @OnClick(R.id.to_discuss) void toDiscuss() {
+    @OnClick(R.id.to_discuss)
+    void toDiscuss() {
         Intent intent = new Intent(this, QueryTopicActivity.class);
         intent.putExtra("courseId", courseId);
         startActivity(intent);
     }
 
-    @OnClick(R.id.to_rank) void toRank() {
+    @OnClick(R.id.to_rank)
+    void toRank() {
         Intent intent = new Intent(this, QuizActivity.class);
         intent.putExtra("courseId", courseId);
         startActivity(intent);
     }
 
-    @OnClick(R.id.to_leave) void toLeave() {
+    @OnClick(R.id.to_leave)
+    void toLeave() {
         Intent intent = new Intent(this, LeaveListActivity.class);
         intent.putExtra("courseId", courseId);
         startActivity(intent);
     }
 
-    @OnClick(R.id.to_seat) void toSeat() {
+    @OnClick(R.id.to_seat)
+    void toSeat() {
         Intent intent = new Intent(this, EnterActivity.class);
         intent.putExtra("courseId", courseId);
         startActivity(intent);
@@ -74,7 +86,16 @@ public class CourseActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
     }
 
-
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
 }
